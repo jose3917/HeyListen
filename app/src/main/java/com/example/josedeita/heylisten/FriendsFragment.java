8b23fdfd40e6b6c11180c0e1aa3cecb086c0c763
kmazas <kevin.mazas.528@my.csun.edu>
2017-10-04 15:40:25 -0700

    List View in Friends Fragment
    Added the List View in fragment_friends.xml, and created an onClick listener for the items, but there's nothing in the method yet.

--- app/src/main/java/com/example/josedeita/heylisten/FriendsFragment.java
+++ app/src/main/java/com/example/josedeita/heylisten/FriendsFragment.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 /**
@@ -22,8 +28,31 @@ public class FriendsFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
+  
+    View view = inflater.inflate(R.layout.fragment_friends, container, false);
+    final String[] friendsList = {"Jose", "Kevin", "Martin", "Sargis"};
+    ListView friendsListView = (ListView) view.findViewById(R.id.friendListView);
+  
+    ArrayAdapter<String> friendsListAdapter = new ArrayAdapter<String>(
+        getActivity(),
+        android.R.layout.simple_list_item_1,
+        friendsList
+    );
+    friendsListView.setAdapter(friendsListAdapter);
+    friendsListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        //This is where we will add code to give
+        //each item in this list some
+        //sort of functionality
+        //Like displaying their phone #
+        //How many times you've run into each other
+        //etc
+      }
+    });
+    
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_friends, container, false);
+    return view;
   }
   
 }
